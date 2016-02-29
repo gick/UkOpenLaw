3445b573b1615f139647033e02e2f96d5ba4b051
gick <gicquel.pierreyves@gmail.com>
2016-02-29 20:48:59 +0100

    Update start.py

--- start.py
+++ start.py
@@ -7,9 +7,9 @@ f= open("Progression.log",'a')
 for currentPage in pageRange:
     try:
         time=datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        f.write("["+time+"]"+" progress " +str(currentPage) +" over " + "5086" + "\n")
+  
         print "Progression" + str(currentPage)+ " sur" + str(5086)
-        response = urllib2.urlopen('http://www.legislation.gov.uk/all/data.feed?page='+str(currentPage))
+        response = urllib2.urlopen('http://www.legislation.gov.uk/') #whatever you want here
         uklaw = response.read()
         soup=BeautifulSoup(uklaw,'xml')
         links=soup.select('link[title="RDF/XML"]')
